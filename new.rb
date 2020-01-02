60bf896fcc135c8a911254a106b3b07c9b088644
Cole Arthur Parham <coleparham@gmail.com>
2020-01-02 20:00:11 +0000

    Automatically backed up by Learn

--- new.rb
+++ new.rb
@@ -2,7 +2,7 @@ src = [
   ["sausage","peppers"],
   ["onions","ketchup"]
 ]
-
+row_index = 0
 3.times do
  puts "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
  return "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
