97af0d14d14a4f0d5a49859c2841f39e91957a6b
Cole Arthur Parham <coleparham@gmail.com>
2020-01-02 19:59:27 +0000

    Automatically backed up by Learn

---
+++ new.rb
@@ -0,0 +1,9 @@
+src = [
+  ["sausage","peppers"],
+  ["onions","ketchup"]
+]
+
+3.times do
+ puts "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+ return "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+ row_index += 1
